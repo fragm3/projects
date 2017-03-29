62e67f0cf2a301e36ec57295ee1b5ae6892da6a4
Shubham Gupta <shubhamg@outlook.com>
2017-03-30 00:39:32 +0530

    First

---
+++ Desktop/PROJECTS/chat-app/server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express= require("express");
+var app= express();
+
+
+
+const publicPath = path.join(__dirname, "../public");
+console.log(__dirname+ '/public' );
+console.log();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () =>{
+	console.log("Server is RUNNING!")
+});
